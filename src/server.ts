0a029474ceb550278492ec65403aeb75c7f4cda3
Rafael V. dos Santos <rvsantos00@gmail.com>
2021-05-09 17:57:41 -0300

    feat: Add create methhod to create categories

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 
+import { categoriesRoutes } from './routes/categories.routes'
+
 const app = express()
 
-app.get('/', (request, response) => response.json({ message: 'Testando aula' }))
+app.use(express.json())
+app.use('/categories', categoriesRoutes)
 
 app.listen(3333, () => console.log('Server is running!'))
