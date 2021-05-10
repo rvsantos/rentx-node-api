bdf8c310cdff2db8135d1db408c3dae44247d939
Rafael V. dos Santos <rvsantos00@gmail.com>
2021-05-09 23:06:47 -0300

    refactor: apply some SOLID principles

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,22 @@
 import { Router } from 'express'
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository'
+import { CreateCategoryService } from '../services/CreateCategoryServicce'
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
 
+// Create Categories
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name)
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists.' })
-  }
-
-  categoriesRepository.create({ name, description })
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
+  createCategoryService.execute({ name, description })
 
   return response.status(201).send()
 })
 
+// List Categories
 categoriesRoutes.get('/', (_request, response) => {
   const categories = categoriesRepository.list()
 
