81f3a901e51845c8a967ce69b747054e98855095
Rafael V. dos Santos <rvsantos00@gmail.com>
2021-05-10 00:31:05 -0300

    feat: add specification create method

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 
 import { categoriesRoutes } from './routes/categories.routes'
+import { specificationsRoutes } from './routes/specifications.routes'
 
 const app = express()
 
 app.use(express.json())
+
+// Routes
 app.use('/categories', categoriesRoutes)
+app.use('/specifications', specificationsRoutes)
 
 app.listen(3333, () => console.log('Server is running!'))
