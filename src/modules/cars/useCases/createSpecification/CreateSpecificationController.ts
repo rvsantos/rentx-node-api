0b6a5168d0fd69de7c47c6fa82b9074a698a9c34
Rafael V. dos Santos <rvsantos00@gmail.com>
2021-05-21 00:04:59 -0300

    wip: CreateSpecifications

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -5,14 +5,14 @@ import { ICreateSpecificationDTO } from '../../repositories/ISpecificaitonsRepos
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase'
 
 class CreateSpecificationController {
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description }: ICreateSpecificationDTO = request.body
 
     const createSpecificationUseCase = container.resolve(
       CreateSpecificationUseCase
     )
 
-    createSpecificationUseCase.execute({ name, description })
+    await createSpecificationUseCase.execute({ name, description })
 
     return response.status(201).send()
   }
